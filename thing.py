57c136f129c6e8d4fbef8e98302f3c3b7c9691ac
Nolan J Crane <NolanC33@github.com>
2021-08-02 16:11:25 -0400

    init commit

---
+++ thing.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+class Checkers(object):
+
+	def draw(self):
+
+		print(self.board)
+
+	def reset(self):
+		board = np.zeros((8, 8))
+
+		for row in range(3):
+			for col in range(8):
+				if (row + col) % 2 == 1:
+					board[row, col] = -1
+
+		for row in reversed(range(5, 8)):
+			for col in range(8):
+				if (row + col) % 2 == 1:
+					board[row, col] = 1
+
+		self.board = board
+			
+	def __init__(self):
+		self.reset()
+		self.draw()
+
+c = Checkers()
